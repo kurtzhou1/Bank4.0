18c92f4a4c4d845b94525c614dfe6f7a422f060e
KurtZhou <pandabat1@gmail.com>
2020-05-29 20:44:31 +0800

    init env

--- src/components/dataMap.js
+++ src/components/dataMap.js
@@ -35,11 +35,35 @@ const DataButtom=()=> {
     <div className='bank40'>
       <div className='bank41'>
         <div className='title'>
-          <div className={index === 0 ? 'show0':'noshow'}>1</div>
-          <div className={index === 1 ? 'show1':'noshow'}>2</div>
-          <div className={index === 2 ? 'show2':'noshow'}>3</div>
-          <div className={index === 3 ? 'show3':'noshow'}>4</div>
-          <div className={index === 4 ? 'show4':'noshow'}>5</div>
+          <div className={index === 0 ? 'show0':'noshow'}>
+            <div>支付</div>
+            <div>轉帳</div>
+            <div>收款</div>
+            <div>繳費</div>
+            <div>無卡提款</div>
+            <div>錢包</div>
+          </div>
+          <div className={index === 1 ? 'show1':'noshow'}>
+            <div>保險</div>
+            <div>失竊險</div>
+            <div>汽、機車強制險</div>
+            <div>旅行平安險</div>
+          </div>
+          <div className={index === 2 ? 'show2':'noshow'}>
+            <div>貸款</div>
+            <div>房屋貸款</div>
+            <div>個人信貸</div>
+          </div>
+          <div className={index === 3 ? 'show3':'noshow'}>
+            <div>籌資</div>
+            <div>股權型群眾募資</div>
+            <div>捐獻型群眾募資</div>
+          </div>
+          <div className={index === 4 ? 'show4':'noshow'}>
+            <div>投資</div>
+            <div>自己選</div>
+            <div>AI智能機器人選股</div>
+          </div>
           <div className={index === 5 ? 'show5':'noshow'}>6</div>
           <div>Bank4.0</div>
         </div>
